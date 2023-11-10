bc702bdee4efd8badbefc668a3f5a36f77db1260
CE LIU <ce.liu@groumimd.com>
2023-11-10 17:49:36 +0900

    feat: bug report, please check next.config.js and the page in pages folder

--- next.config.js
+++ next.config.js
@@ -1,4 +1,42 @@
 /** @type {import("next").NextConfig} */
-module.exports = {
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  worker-src 'self' blob:;
+  style-src 'self' 'unsafe-inline' https://heapanalytics.com;
+  font-src 'self';
+  object-src 'none';
+  img-src 'self';
+  connect-src 'self';
+`;
+
+const securityHeaders = [
+{
+  key: "Strict-Transport-Security",
+  value: "max-age=63072000; includeSubDomains; preload",
+},
+];
+
+securityHeaders.push({
+  key: "Content-Security-Policy",
+  value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+});
+
+const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
+  i18n: {
+    locales: ['en-US', 'fr', 'nl-NL'],
+    defaultLocale: 'en-US',
+  },
+  
 }
+
+module.exports = nextConfig
